34db3aefc89d0dfd1cd563518d32b2a953b784e6
aguilarivan <ivanaguilarnardin@gmail.com>
2025-04-23 22:12:26 -0300

    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = `https://example-backend.railway.internal:8080/boxes`
+const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/boxes`
+console.log('NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL);
+
 
 export interface Box {
   id?: number;
