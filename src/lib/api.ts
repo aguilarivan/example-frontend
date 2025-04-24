4224f65d9c7cb11e78019b13edd34abf5bc377bd
aguilarivan <ivanaguilarnardin@gmail.com>
2025-04-23 22:19:35 -0300

    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const API_URL = `https://${process.env.NEXT_PUBLIC_API_URL}/boxes`
-console.log('NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL);
 
 
 export interface Box {
